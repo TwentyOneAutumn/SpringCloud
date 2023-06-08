4fa6e3f196e67741c60ea16bb822dcc6515300c2
thetwentyoneautumn <920614827@qq.com>
2023-06-08 17:32:23 +0800

    第二十一个秋天 2023-06-01 完善Security 提交代码

--- DataBaseConfig/src/main/java/com/database/config/TransactionManagementConfig.java
+++ DataBaseConfig/src/main/java/com/database/config/TransactionManagementConfig.java
@@ -1,31 +1,30 @@
 //package com.core.config;
 //
 //import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
 //import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 //import org.springframework.data.transaction.ChainedTransactionManager;
 //import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 //import org.springframework.stereotype.Component;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
 //import javax.sql.DataSource;
 //
 ///**
 // * 事务管理器配置类
 // */
-//@Component
+//@Configuration
 //public class TransactionManagementConfig {
-//
-//    @Bean
-//    DataSourceTransactionManager oneDataSourceTransactionManager(@Qualifier("oneDataSource") DataSource dataSource){
-//        return new DataSourceTransactionManager(dataSource);
-//    }
 //
 //    /**
 //     * 联合事务管理器
 //     * 已弃用
-//     * @param oneDataSourceTransactionManager 事务管理器
+//     * @param dataSource 数据源
 //     * @return Bean
 //     */
 //    @Bean
-//    ChainedTransactionManager chainedDataSourceTransactionManager(@Qualifier("oneDataSourceTransactionManager") DataSourceTransactionManager oneDataSourceTransactionManager){
-//        return new ChainedTransactionManager(oneDataSourceTransactionManager);
+//    PlatformTransactionManager platformTransactionManager(@Qualifier("oneDataSource") DataSource dataSource){
+//        return new ChainedTransactionManager(new DataSourceTransactionManager(dataSource));
 //    }
 //}
