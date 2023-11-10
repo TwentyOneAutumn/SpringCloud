017c683f62bce341f517d8398b5741d1a59720be
maplej <920614827@qq.com>
2023-11-10 15:17:19 +0800

    2023-11-10 优化代码生成器

--- generator/src/main/java/com/generator/controller/GeneratorController.java
+++ generator/src/main/java/com/generator/controller/GeneratorController.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.sql.DataSource;
 import javax.validation.Valid;
 import java.io.File;
